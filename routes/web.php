cec65ecc5b40dcb0782c86cfbf1cf864a533beab
UsmanSiddique1 <usmansiddique406@gmail.com>
2023-05-28 00:42:10 +0500

    login chagnes

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ use App\Http\Controllers\ReceptionistController;
 //     return view('welcome');
 // });
 
-Auth::routes();
+Auth::routes(['register' => false]);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::group(['middleware'=> ['auth']], function(){
