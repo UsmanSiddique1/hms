b88aad86d3eaa623b1e9c5b784287303fa28e282
UsmanSiddique1 <47412667+UsmanSiddique1@users.noreply.github.com>
2024-04-30 05:57:11 +0500

    patients page and create slip page ajax filters

--- app/Services/PatientService.php
+++ app/Services/PatientService.php
@@ -10,7 +10,48 @@ class PatientService{
 
     public function getAll()
     {
-        return Patient::all();
+        return $this->queryBuilder()->get();
+    }
+
+    public function queryBuilder()
+    {
+        return Patient::query();
+    }
+
+    public function store($request)
+    {
+        return Patient::create($request);
+    }
+
+    public function update($request, $brand)
+    {
+        return $brand->update($request);
+    }
+
+    public function showOne($brand)
+    {
+        return $brand;
+    }
+
+    public function edit($brand)
+    {
+        return $brand;
+    }
+
+    public function delete($brand)
+    {
+        return $brand->delete();
+    }
+
+    public function filters($request)
+    {
+        $patients = $this->queryBuilder();
+        isset($request->mr_number) && $request->mr_number != '' ? $patients->where('mr_number', $request->mr_number) : '';
+        isset($request->cnic) && $request->cnic != '' ? $patients->where('cnic', $request->cnic) : '';
+        isset($request->phone) && $request->phone != '' ? $patients->where('phone', $request->phone) : '';
+        isset($request->name) && $request->name != '' ? $patients->where('name', $request->name) : '';
+        isset($request->address) && $request->address != '' ? $patients->where('address', $request->address) : '';
+        return $patients;
     }
 
 }
--- resources/views/patients/index.blade.php
+++ resources/views/patients/index.blade.php
@@ -6,7 +6,7 @@
             <div class="col-lg-6 col-md-6 col-sm-12">
                 <h2>All Patient</h2>
                 <ul class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="index.html"><i class="fa fa-dashboard"></i></a></li>                            
+                    <li class="breadcrumb-item"><a href="index.html"><i class="fa fa-dashboard"></i></a></li>
                     <li class="breadcrumb-item">Patient</li>
                     <li class="breadcrumb-item active">All Patient</li>
                 </ul>
@@ -15,10 +15,10 @@
                 <div class="d-flex flex-row-reverse">
                     <div class="page_action">
                         <a href="{{ route('patients.create') }}" class="btn btn-secondary">Add Patient</a>
-                        
+
                     </div>
                     <div class="p-2 d-flex">
-                        
+
                     </div>
                 </div>
             </div>
@@ -30,14 +30,13 @@
             <div class="card patients-list">
                 <div class="header">
                     <h2>Patients List</h2>
-                    
+
                 </div>
                 <div class="body">
                     <div class="table-responsive">
-                        <table class="table table-bordered table-striped table-hover dataTable js-exportable">
+                        <table id="data-table" class="table table-bordered table-striped table-hover">
                             <thead>
-                                <tr>                                       
-                                    <th>Media</th>
+                                <tr>
                                     <th>MR Number</th>
                                     <th>Name</th>
                                     <th>CNIC</th>
@@ -47,39 +46,11 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                @foreach($patients as $patient)
-                                <tr>
-                                    <td><span class="list-icon"><img class="avatar" src="../assets/images/xs/avatar1.jpg" alt=""></span></td>
-                                    <td><span class="list-name">{{ $patient->mr_number }}</span></td>
-                                    <td>{{ $patient->name }}</td>
-                                    <td>{{ $patient->cnic }}</td>
-                                    <td>{{ $patient->age_years }}y , {{ $patient->age_months }}m, {{ $patient->age_weeks }}w</td>
-                                    <td>{{ $patient->phone }}</td>
-                                    <td>
-                                        <button type="button" class="btn btn-primary edit-patient"
-                                        data-id="{{ $patient->id }}" 
-                                        data-mr_number="{{ $patient->mr_number }}" 
-                                        data-name="{{ $patient->name }}" 
-                                        data-age_years="{{ $patient->age_years }}" 
-                                        data-age_months="{{ $patient->age_months }}" 
-                                        data-age_weeks="{{ $patient->age_weeks }}" 
-                                        data-gender="{{ $patient->gender }}" 
-                                        data-phone="{{ $patient->phone }}" 
-                                        >
-                                        <i class="fa fa-pencil"></i>
-                                          </button>
-                                          <a href="javascript:void(0)" class="delete btn btn-danger" 
-                                          data-id="{{ $patient->id }}"
-                                          data-name="{{ $patient->name }}"
-                                            ><i class="fa fa-trash"></i></a>
-                                    </td>
-                                </tr>
-                                @endforeach
-                                
+
                             </tbody>
-                        </table>                            
+                        </table>
                     </div>
-                    
+
                 </div>
             </div>
         </div>
@@ -90,87 +61,173 @@
 <!-- Modal -->
 <div class="modal fade" id="updateModel" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form id="updateForm" method="POST">
+                @csrf
+                @method('PUT')
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="">Name</label>
+                        <input type="text" class="form-control" name="name" id="name" placeholder="Name">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Phone</label>
+                        <input type="text" class="form-control" name="phone" id="phone" disabled placeholder="phone">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Cnic</label>
+                        <input type="text" class="form-control" name="cnic" id="phone" disabled placeholder="phone">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Age Years</label>
+                        <input type="text" class="form-control" name="age_years" id="age_years" placeholder="Name">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Age Months</label>
+                        <input type="text" class="form-control" name="age_months" id="age_months" placeholder="Name">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Age Weeks</label>
+                        <input type="text" class="form-control" name="age_weeks" id="age_weeks" placeholder="Name">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Gender</label>
+                        <select name="gender" id="gender" class="form-control">
+                            <option value="">Select Gender</option>
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                        </select>
+                    </div>
+
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Save changes</button>
+                </div>
+            </form>
         </div>
-        <form id="updateForm" method="POST">
-        @csrf
-        @method('PUT')
-            <div class="modal-body">                
-                <div class="form-group">
-                    <label for="">Name</label>
-                    <input type="text" class="form-control" name="name" id="name" placeholder="Name">
-                </div>
-                <div class="form-group">
-                    <label for="">Phone</label>
-                    <input type="text" class="form-control" name="phone" id="phone" disabled placeholder="phone">
-                </div>
-                <div class="form-group">
-                    <label for="">Cnic</label>
-                    <input type="text" class="form-control" name="cnic" id="phone" disabled placeholder="phone">
-                </div>
-                <div class="form-group">
-                    <label for="">Age Years</label>
-                    <input type="text" class="form-control" name="age_years" id="age_years" placeholder="Name">
-                </div>
-                <div class="form-group">
-                    <label for="">Age Months</label>
-                    <input type="text" class="form-control" name="age_months" id="age_months" placeholder="Name">
-                </div>
-                <div class="form-group">
-                    <label for="">Age Weeks</label>
-                    <input type="text" class="form-control" name="age_weeks" id="age_weeks" placeholder="Name">
-                </div>
-                <div class="form-group">
-                    <label for="">Gender</label>
-                    <select name="gender" id="gender" class="form-control">
-                        <option value="">Select Gender</option>
-                        <option value="male">Male</option>
-                        <option value="female">Female</option>
-                    </select>
-                </div>
-                
-                
-            </div>
-            <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="submit" class="btn btn-primary">Save changes</button>
-            </div>
-        </form>
-      </div>
     </div>
-  </div>
-  <div class="modal fade" id="deleteModel" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+</div>
+<div class="modal fade" id="deleteModel" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Delete Patient <strong>"<span id="patient_name"></span>"</strong> </h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Delete Patient <strong>"<span id="patient_name"></span>"</strong> </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form id="deleteForm" method="POST" enctype="multipart/form-data">
+                @csrf
+                @method('DELETE')
+                <div class="modal-body">
+                    <strong>Alert!</strong> This Action will not be UNDO.
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">DELETE NOW</button>
+                </div>
+            </form>
         </div>
-        <form id="deleteForm" method="POST" enctype="multipart/form-data">
-            @csrf
-            @method('DELETE')
-            <div class="modal-body"> 
-                <strong>Alert!</strong> This Action will not be UNDO.
-                
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="submit" class="btn btn-primary">DELETE NOW</button>
-            </div>
-        </form>
-      </div>
     </div>
 </div>
 @push('footer-scripts')
 <script>
-    $('.edit-patient').click(function(){
+    var table = $('#data-table').DataTable({
+        processing: true,
+        serverSide: true,
+        ajax: {
+            url: "{{ route('patients.datatable') }}",
+            method: "GET",
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            data: function(d) {
+                d.doctor_id = $('#data-table').data('doctor_id');
+                d.patient_id = $('#data-table').data('patient_id');
+                d.receptionist_id = $('#data-table').data('receptionist_id');
+                d.slip_id = $('#data-table').data('slip_id');
+                d.from_date = $('#data-table').data('from_date');
+                d.to_date = $('#data-table').data('to_date');
+                d.type = $('#data-table').data('type');
+            }
+        },
+        columns: [{
+                data: 'mr_number',
+                name: 'mr_number'
+            },
+            {
+                data: 'name',
+                name: 'name'
+            },
+            {
+                data: 'cnic',
+                name: 'cnic'
+            },
+            {
+                data: 'phone',
+                name: 'phone'
+            },
+            {
+                data: 'age',
+                name: 'age'
+            },
+            {
+                data: 'action',
+                name: 'action',
+                orderable: false,
+                searchable: false
+            },
+        ]
+    });
+    $("#doctor").change(function() {
+        $('#data-table').data('doctor_id', $(this).val());
+        table.ajax.reload(null, false);
+    });
+    $("#patient").change(function() {
+        $('#data-table').data('patient_id', $(this).val());
+        table.ajax.reload(null, false);
+    });
+    $("#slip").change(function() {
+        $('#data-table').data('slip_id', $(this).val());
+        table.ajax.reload(null, false);
+    });
+    $("#receptionist").change(function() {
+        $('#data-table').data('receptionist_id', $(this).val());
+        table.ajax.reload(null, false);
+    });
+    $("#type").change(function() {
+        $('#data-table').data('type', $(this).val());
+        table.ajax.reload(null, false);
+    });
+    $("#from_date").change(function() {
+        $('#data-table').data('from_date', $(this).val());
+        table.ajax.reload(null, false);
+    });
+    $("#to_date").change(function() {
+        $('#data-table').data('to_date', $(this).val());
+        table.ajax.reload(null, false);
+    });
+    $('#clear-filter').click(function() {
+        $('#doctor').val('').trigger('change');
+        $('#patient').val('').trigger('change');
+        $('#slip').val('').trigger('change');
+        $('#receptionist').val('').trigger('change');
+        $('#from_date').val('');
+        $('#to_date').val('');
+        table.ajax.reload(null, false);
+    });
+
+    $('.edit-patient').click(function() {
+        console.log("delete "+$(this).data("mr_number"));
         $('.modal-title').text($(this).data("mr_number"));
         $('#name').val($(this).data("name"));
         $('#phone').val($(this).data("phone"));
@@ -179,17 +236,46 @@
         $('#age_months').val($(this).data("age_months"));
         $('#age_weeks').val($(this).data("age_weeks"));
         var patient_id = $(this).data('id');
-        $('#updateForm').attr('action',"{{ url('patients') }}/"+patient_id);
+        $('#updateForm').attr('action', "{{ url('patients') }}/" + patient_id);
         $('#updateModel').modal().show();
     });
 
-    $('.delete').click(function(){
-        var id = $(this).data('id');
-        var name = $(this).data('name');
-        $('#patient_name').text(name);
-        $('#deleteForm').attr('action',"{{ url('patients') }}/"+id);
-        $('#deleteModel').modal().show();
-    });
+    // Sweet Alert on delete
+    function deleteItem(itemId) {
+        console.log("delete: " + itemId);
+        Swal.fire({
+            title: 'Are you sure?',
+            text: 'You will not be able to recover this item!',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#d33',
+            cancelButtonColor: '#3085d6',
+            confirmButtonText: 'Yes, delete it!',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                console.log(itemId);
+                $.ajax({
+                    url: 'slips/' + itemId,
+                    type: 'DELETE',
+                    data: {
+                        _token: '{{ csrf_token() }}',
+                    },
+                    success: function(data) {
+                        table.ajax.reload(null, false);
+                        Swal.fire('Success', 'Slip has been deleted successfully!', 'success')
+                    },
+                    error: function(xhr) {
+                        Swal.fire('Error', 'Error in deleting slip !', 'success')
+
+                    }
+                });
+            }
+        });
+    }
+
+    $(".select2").select2();
+
+
 </script>
 @endpush
 @endsection
